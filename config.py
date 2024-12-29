4607f540e4ef5335553be6d1d75afee18ffe8dfb
Noobinvester <135937970+Noobinvester@users.noreply.github.com>
2024-12-29 12:22:19 -0800

    Create config.py
    This works a long side the main code, you will need to modify this to match your reddit account info

---
+++ config.py
@@ -0,0 +1,4 @@
+username = "reddit username"
+password = "reddit password"
+client_id = "app ID after you create your app in reddit prefs apps"
+client_secret = "app secret key after you create your app in reddit prefs apps"
